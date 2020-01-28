3ed3ec7c32d9e68099b41de0f856c6b5d1ce7c11
sidhujag <jsidhu@blockchainfoundry.co>
2020-01-27 20:10:48 -0800

    compile

--- db/rocksdb_syscointype.go
+++ db/rocksdb_syscointype.go
@@ -88,7 +88,7 @@ func (d *RocksDB) ConnectAssetOutput(sptData []byte, balances map[string]*AddrBa
 			balance.BalanceAssetUnAllocatedSat = map[uint32]big.Int{}
 		}
 		balanceTransfer.BalanceAssetUnAllocatedSat[assetGuid] = balance.BalanceAssetUnAllocatedSat[assetGuid]
-		balance.BalanceAssetUnAllocatedSat[assetGuid].Set(big.NewInt(0))
+		balance.BalanceAssetUnAllocatedSat[assetGuid] = big.NewInt(0)
 	} else {
 		if balance.BalanceAssetUnAllocatedSat == nil{
 			balance.BalanceAssetUnAllocatedSat = map[uint32]big.Int{}
@@ -383,7 +383,7 @@ func (d *RocksDB) DisconnectAssetOutput(sptData []byte, balances map[string]*Add
 		}
 		// transfer values back to original owner and 0 out the transferee balance
 		balance.BalanceAssetUnAllocatedSat[assetGuid] = balanceTransfer.BalanceAssetUnAllocatedSat[assetGuid]
-		balanceTransfer.BalanceAssetUnAllocatedSat[assetGuid].Set(big.NewInt(0))
+		balanceTransfer.BalanceAssetUnAllocatedSat[assetGuid] = big.NewInt(0)
 	} else if balance.SentAssetUnAllocatedSat != nil {
 		sentAssetUnAllocatedSat := balance.SentAssetUnAllocatedSat[assetGuid]
 		balanceAssetUnAllocatedSat, ok := balance.BalanceAssetUnAllocatedSat[assetGuid]
