d1d533b211b550d105f4a5a34d69571d1c6ffd12
sidhujag <jsidhu@blockchainfoundry.co>
2020-01-28 18:13:15 -0800

    use ptrs for big int

--- db/rocksdb_syscointype.go
+++ db/rocksdb_syscointype.go
@@ -100,7 +100,6 @@ func (d *RocksDB) ConnectAssetOutput(sptData []byte, balances map[string]*bchain
 		}
 		valueSat := big.NewInt(asset.Balance)
 		balanceAssetUnAllocatedSat.Add(balanceAssetUnAllocatedSat, valueSat)
-		balance.BalanceAssetUnAllocatedSat[assetGuid] = balanceAssetUnAllocatedSat
 		if assetGuid == 135354521 {
 			glog.Warningf("asset tx %v assetGuid balance %v", assetGuid, balance.BalanceAssetUnAllocatedSat[assetGuid])
 			testbalance, _ := balances[senderStr]
