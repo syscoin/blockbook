611a68ee0bc4ba120a6affc9768942a538e04f84
sidhujag <jsidhu@blockchainfoundry.co>
2020-01-28 18:25:30 -0800

    comile

--- bchain/coins/sys/syscoinparser.go
+++ bchain/coins/sys/syscoinparser.go
@@ -177,29 +177,29 @@ func (p *SyscoinParser) UnpackAddrBalance(buf []byte, txidUnpackedLen int, detai
 	ab.SentAssetAllocatedSat = map[uint32]*big.Int{}
 	for i := uint(0); i < numSentAssetAllocatedSat; i++ {
 		key, l := p.BaseParser.UnpackVaruint(buf[l:])
-		value, l := &p.BaseParser.UnpackBigint(buf[l:])
-		ab.SentAssetAllocatedSat[uint32(key)].Set(value)
+		value, l := p.BaseParser.UnpackBigint(buf[l:])
+		ab.SentAssetAllocatedSat[uint32(key)] = &value
 	}
 	numBalanceAssetAllocatedSat, l := p.BaseParser.UnpackVaruint(buf[l:])
 	ab.BalanceAssetAllocatedSat = map[uint32]*big.Int{}
 	for i := uint(0); i < numBalanceAssetAllocatedSat; i++ {
 		key, l := p.BaseParser.UnpackVaruint(buf[l:])
-		value, l := &p.BaseParser.UnpackBigint(buf[l:])
-		ab.BalanceAssetAllocatedSat[uint32(key)].Set(value)
+		value, l := p.BaseParser.UnpackBigint(buf[l:])
+		ab.BalanceAssetAllocatedSat[uint32(key)] = &value
 	}
 	numSentAssetUnAllocatedSat, l := p.BaseParser.UnpackVaruint(buf[l:])
 	ab.SentAssetUnAllocatedSat = map[uint32]*big.Int{}
 	for i := uint(0); i < numSentAssetUnAllocatedSat; i++ {
 		key, l := p.BaseParser.UnpackVaruint(buf[l:])
-		value, l := &p.BaseParser.UnpackBigint(buf[l:])
-		ab.SentAssetUnAllocatedSat[uint32(key)].Set(value)
+		value, l := p.BaseParser.UnpackBigint(buf[l:])
+		ab.SentAssetUnAllocatedSat[uint32(key)] = &value
 	}
 	numBalanceAssetUnAllocatedSat, l := p.BaseParser.UnpackVaruint(buf[l:])
 	ab.BalanceAssetUnAllocatedSat = map[uint32]*big.Int{}
 	for i := uint(0); i < numBalanceAssetUnAllocatedSat; i++ {
 		key, l := p.BaseParser.UnpackVaruint(buf[l:])
-		value, l := &p.BaseParser.UnpackBigint(buf[l:])
-		ab.BalanceAssetUnAllocatedSat[uint32(key)].Set(value)
+		value, l := p.BaseParser.UnpackBigint(buf[l:])
+		ab.BalanceAssetUnAllocatedSat[uint32(key)] = &value
 	}	
 	if numSentAssetAllocatedSat > 0 || numBalanceAssetAllocatedSat > 0 ||  numSentAssetUnAllocatedSat > 0 || numBalanceAssetUnAllocatedSat > 0 {
 		glog.Warningf("UnpackAddrBalance numSentAssetAllocatedSat %v numBalanceAssetAllocatedSat %v numSentAssetUnAllocatedSat %v numBalanceAssetUnAllocatedSat %v",numSentAssetAllocatedSat, numBalanceAssetAllocatedSat, numSentAssetUnAllocatedSat, numBalanceAssetUnAllocatedSat)
