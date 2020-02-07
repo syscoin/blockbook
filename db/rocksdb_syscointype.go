1ebb04ee6a13ff536e0ffc2d7308213fbd2b0afa
sidhujag <jsidhu@blockchainfoundry.co>
2020-02-06 21:47:56 -0800

    dbg

--- db/rocksdb_syscointype.go
+++ db/rocksdb_syscointype.go
@@ -880,6 +880,8 @@ func (d *RocksDB) GetAsset(guid uint32, assets *map[uint32]*bchain.Asset) (*bcha
 func (d *RocksDB) storeTxAssets(wb *gorocksdb.WriteBatch, txassets map[string]*bchain.TxAsset) error {
 	for _, txAsset := range txassets {
 		key := d.chainParser.PackAssetKey(txAsset.AssetGuid, txAsset.Height)
+		guidUnpacked, heightUnpacked := d.chainParser.UnpackAssetKey(key)
+		glog.Warningf("rocksdb: storeTxAssets guid %v height %v unpacked guid %v unpacked height %v",  txAsset.AssetGuid, txAsset.Height, guidUnpacked, heightUnpacked)
 		buffer := &bytes.Buffer{}
 		gob.NewEncoder(buffer).Encode(txAsset.Txids)
 		wb.PutCF(d.cfh[cfTxAssets], key, buffer.Bytes())
