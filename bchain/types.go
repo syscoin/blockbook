f06617b432f0d5f8457bbc328be64704909c0639
sidhujag <jsidhu@blockchainfoundry.co>
2020-02-20 11:15:48 -0800

    c

--- bchain/types.go
+++ bchain/types.go
@@ -460,7 +460,7 @@ type Token struct {
 type TokenTransferRecipient struct {
 	To       string    `json:"to"`
 	Value    *Amount   `json:"value"`
-	bool	 Unspent   `json:"-"`
+	Unspent  bool      `json:"-"`
 }
 // TokenTransferSummary contains info about a token transfer done in a transaction
 type TokenTransferSummary struct {
