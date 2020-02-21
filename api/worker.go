f872f6023a4fcdeaf1ea131dc3694b0dad5632d8
sidhujag <jsidhu@blockchainfoundry.co>
2020-02-20 16:47:20 -0800

    c

--- api/worker.go
+++ api/worker.go
@@ -949,7 +949,7 @@ func (w *Worker) GetAddress(address string, page int, txsOnPage int, option Acco
 }
 func (w *Worker) FindAssets(assetSymbol string) []*Asset {
 	start := time.Now()
-	assets := w.db.findAssetsFromSymbol(assetSymbol)
+	assets := w.db.FindAssetsFromSymbol(assetSymbol)
 	glog.Info("FindAssets ", asset, " finished in ", time.Since(start))
 	return assets
 }
--- db/rocksdb_syscointype.go
+++ db/rocksdb_syscointype.go
@@ -822,7 +822,7 @@ func (d *RocksDB) DisconnectSyscoinOutputs(height uint32, btxID []byte, addrDesc
 	return err
 }
 // find assets from cache that contain symbol
-func (d *RocksDB) findAssetsFromSymbol(symbol string) []*bchain.Asset {
+func (d *RocksDB) FindAssetsFromSymbol(symbol string) []*bchain.Asset {
 	assets := make([]*bchain.Asset, 0)
 	for _, asset := range AssetCache {
 		if strings.Contains(asset.AssetObj.Symbol, symbol) {
