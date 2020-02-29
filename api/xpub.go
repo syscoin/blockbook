35417a330f545671c9c8bd9671869c139927b8cf
sidhujag <jsidhu@blockchainfoundry.co>
2020-02-28 19:58:34 -0800

    fix nil check for xpub token

--- api/xpub.go
+++ api/xpub.go
@@ -293,6 +293,9 @@ func (w *Worker) tokenFromXpubAddress(data *xpubData, ad *xpubAddress, changeInd
 				})
 			}
 			sort.Sort(tokens)
+		} else {
+			// ensures balance won't be nil and thus seen as "used" in calling functions (no need to check for ad.Balances != nil but check for tokens.BalanceSat is nil (not used, or 0 if it is used but option < AccountDetailsTokenBalances)
+			balance = big.NewInt(0)
 		}
 	}
 	// for base token
