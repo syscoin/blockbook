6824fb1e8fac2c9f9e32bbf90702f66f24448fd6
sidhujag <jsidhu@blockchainfoundry.co>
2020-03-02 13:04:22 -0800

    c

--- bchain/coins/blockchain.go
+++ bchain/coins/blockchain.go
@@ -314,7 +314,7 @@ func (c *blockChainWithMetrics) EthereumTypeGetErc20ContractBalance(addrDesc, co
 
 func (c *blockChainWithMetrics) AssetAllocationSend(asset int, sender string, receiver string, amount string) (rawtx string, err error) {
 	defer func(s time.Time) { c.observeRPCLatency("AssetAllocationSend", s, err) }(time.Now())
-	rawtx, err := c.b.AssetAllocationSend(asset, sender, receiver, amount)
+	rawtx, err = c.b.AssetAllocationSend(asset, sender, receiver, amount)
 	return rawtx, err
 }
 
