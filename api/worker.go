5a819cca59ac8df6019649fe8774c66610ddaec3
Martin Boehm <martin.boehm@1mbsoftware.net>
2020-12-13 22:19:14 +0100

    Return info about backend in websocket getInfo request

--- api/worker.go
+++ api/worker.go
@@ -2362,7 +2362,7 @@ func (w *Worker) GetSystemInfo(internal bool) (*SystemInfo, error) {
 		DbColumns:         columnStats,
 		About:             Text.BlockbookAbout,
 	}
-	backendInfo := &BackendInfo{
+	backendInfo := &common.BackendInfo{
 		BackendError:    backendError,
 		BestBlockHash:   ci.Bestblockhash,
 		Blocks:          ci.Blocks,
@@ -2377,6 +2377,7 @@ func (w *Worker) GetSystemInfo(internal bool) (*SystemInfo, error) {
 		Warnings:        ci.Warnings,
 		Consensus:       ci.Consensus,
 	}
+	w.is.SetBackendInfo(backendInfo)
 	glog.Info("GetSystemInfo finished in ", time.Since(start))
 	return &SystemInfo{blockbookInfo, backendInfo}, nil
 }
