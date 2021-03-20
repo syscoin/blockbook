4ad0162cf15bf4fd5352f221332ddfde91ec160b
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-03-19 21:57:28 -0700

    Update worker.go

--- api/worker.go
+++ api/worker.go
@@ -1462,7 +1462,6 @@ func (w *Worker) GetAsset(asset string, page int, txsOnPage int, option AccountD
 	} else {
 		totalResults = -1
 	}
-	tokenMempoolInfo := TokenMempoolInfo{UnconfirmedTxs: 0, ValueSat: &big.Int{}}
 	// process mempool, only if toHeight is not specified
 	if filter.ToHeight == 0 && !filter.OnlyConfirmed {
 		txm, err = w.getAssetTxids(assetGuid, true, filter, maxInt)
