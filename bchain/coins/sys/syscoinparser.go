fb5f10f33c6e70daf253ebad2ee4c5a266226edc
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-05-23 15:32:56 -0700

    check if memo len > 0 not just for nil

--- bchain/coins/sys/syscoinparser.go
+++ bchain/coins/sys/syscoinparser.go
@@ -391,11 +391,10 @@ func (p *SyscoinParser) PackAssetAllocationMemoKey(assetGuid uint64, addrDesc *b
 }
 
 func (p *SyscoinParser) UnpackAssetAllocationMemo(buf []byte) *bchain.AssetAllocationMemo {
-	ll := 0
 	initialMemo, ll := p.BaseParser.UnpackVarBytes(buf)
 	mostRecentMemo, l := p.BaseParser.UnpackVarBytes(buf[ll:])
 	ll += l
-	prevMemo, l := p.BaseParser.UnpackVarBytes(buf[ll:])
+	prevMemo, _ := p.BaseParser.UnpackVarBytes(buf[ll:])
 	return &bchain.AssetAllocationMemo{InitialMemo: initialMemo, MostRecentMemo: mostRecentMemo, PrevMemo: prevMemo}
 }
 
