3de60bcfb38204ff6188ed6b68fcce3aef6bcd17
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-05-30 14:16:25 -0700

    Update syscoinparser.go

--- bchain/coins/sys/syscoinparser.go
+++ bchain/coins/sys/syscoinparser.go
@@ -662,7 +662,7 @@ func (p *SyscoinParser) PackTxIndexes(txi []bchain.TxIndexes) []byte {
 }
 
 func (p *SyscoinParser) PackAsset(asset *bchain.Asset) ([]byte, error) {
-	var buf []byte
+	buf := make([]byte, 0, 53)
 	varBuf := make([]byte, 4)
 	l := p.BaseParser.PackVaruint(uint(asset.Transactions), varBuf)
 	buf = append(buf, varBuf[:l]...)
