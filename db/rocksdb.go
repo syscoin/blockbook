30211c75877c2264e5875ca5ff5a3271b513eded
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-05-31 10:10:51 -0700

    Update rocksdb.go

--- db/rocksdb.go
+++ db/rocksdb.go
@@ -749,7 +749,7 @@ func (d *RocksDB) processAddressesBitcoinType(block *bchain.Block, addresses bch
 						balanceAsset = &bchain.AssetBalance{Transfers: 0, BalanceSat: big.NewInt(0), SentSat: big.NewInt(0)}
 						balance.AssetBalances[spentOutput.AssetInfo.AssetGuid] = balanceAsset
 					}
-					err := d.ConnectAllocationInput(&spentOutput.AddrDesc, block.Height, tx.Version, balanceAsset, spendingTxid, spentOutput.AssetInfo, blockTxAssetAddresses, assets, txAssets, tx.memo)
+					err := d.ConnectAllocationInput(&spentOutput.AddrDesc, block.Height, tx.Version, balanceAsset, spendingTxid, spentOutput.AssetInfo, blockTxAssetAddresses, assets, txAssets, tx.Memo)
 					if err != nil {
 						return err
 					}
